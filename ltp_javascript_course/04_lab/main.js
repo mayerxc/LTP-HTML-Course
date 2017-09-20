4d66f304aeb100c6d756efe90af8b4e80a1f1bb1
Chris Mayer <mayerxc@gmail.com>
2017-09-20 11:16:22 -0400

    adding lab 4

---
+++ ltp_javascript_course/04_lab/main.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    document.getElementById('findNameButton').addEventListener('click', lookForName);
+    
+    
+    function lookForName() {
+        var myName = 'Chris';
+        var timesFound = [];
+        var text = document.getElementById('inputNameBox').value;
+        console.log(text);
+        
+        for (var i = 0; text.length > i; i++) {
+            if (text[i] === "C"){
+                console.log("Made it inside loop!")
+                for (var j = i; j < (myName.length + i); j++){
+                    timesFound.push(text[j]);
+                }
+            }
+        }
+        if (timesFound.length === 0){
+            alert("Your name wasn't found!");
+        } else {
+            alert(timesFound);
+        }
+    }
+}
