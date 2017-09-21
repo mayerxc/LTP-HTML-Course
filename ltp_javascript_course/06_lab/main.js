58dd447279bb5b1d5c2f9f3e9192491851f8bca7
Chris Mayer <mayerxc@gmail.com>
2017-09-21 13:49:18 -0400

    finishing lab 6

--- ltp_javascript_course/06_lab/main.js
+++ ltp_javascript_course/06_lab/main.js
@@ -1,10 +1,20 @@
-var randomBox1 = document.getElementById('eventBox1');
-var randomBox2 = document.getElementById("eventBox2");
-
-randomBox2.onmouseover = function() {
-    alert("you hovered over the red box.");
+window.onload = function() {
+    var randomBox1 = document.getElementById('eventBox1');
+    var randomBox2 = document.getElementById("eventBox2");
+    console.log(randomBox2);
     
+    
+    randomBox2.onmouseover = function() {
+        alert("you hovered over the red box.");
+
+    }
+    randomBox1.onclick = function(e) {
+        alert("X=" + e.clientX + " and Y=" + e.clientY);
+    }    
+    
+    window.onkeypress = function(e){
+        console.log(e);
+        alert('You pressed the '+ e.key + ' key!');
+    }
 }
-randomBox1.onclick = function(e) {
-    alert("X=" + e.clientX + " and Y=" + e.clientY);
-}
+
