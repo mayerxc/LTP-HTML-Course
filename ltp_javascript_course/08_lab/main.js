f33f41a376bd1108a19ef5f077d571dbba0699d6
Chris Mayer <mayerxc@gmail.com>
2017-09-21 20:01:19 -0400

    adding some of lab 8

---
+++ ltp_javascript_course/08_lab/main.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    var arr = ["Sap", "Haze", "Julius", "Doppelganger", "Good Morning", "Julius", "Julius", "Haze"];
+    var mostFrequent;
+    var notFrequent = 0;
+    var item;
+    
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                notFrequent++;
+            }
+            if 
+        }
+    }
+}
