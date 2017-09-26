6fba55953ea59f94570a273442249c069f26aec0
Chris Mayer <mayerxc@gmail.com>
2017-09-26 19:53:43 -0400

    adding php lab 3

---
+++ ltp_php_course/03_lab/03_lab.php
@@ -0,0 +1,18 @@
+<?php
+    $fullName = "Chris Mayer";
+    $yearOfBirth = 1978;
+    $taxRate = .37;
+    $likePickles = true;
+    $newTaxRate = .395;
+
+    print("Full Name: " . $fullName);
+    print("<br/>");
+    print("Year born: " . $yearOfBirth);
+    print("<br/>");
+    print("Tax rate: " . $taxRate);
+    print("<br/>");
+    print("Like Pickles: " . $likePickles);
+    print("<br/>");
+    print("New Tax Rate: " . $newTaxRate);
+
+?>
