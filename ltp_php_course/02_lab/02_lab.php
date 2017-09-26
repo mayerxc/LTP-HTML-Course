c8b6929f69b67ce3a06180ccc145b72fd440cd39
Chris Mayer <mayerxc@gmail.com>
2017-09-26 15:55:56 -0400

    adding lab 2 for php

---
+++ ltp_php_course/02_lab/02_lab.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Doing some math</title>
+</head>
+
+<body>
+    <p>
+        27 * 3 = <?php print(27 * 3); ?>
+        <br/>
+        19 / 5 = <?php print(19 / 5); ?>
+        <br/>
+        22 + 3 + 21 = <?php print(22 + 3 + 21); ?>
+        <br/>
+        18 + 2 * 2 = <?php print(18 + 2 * 2); ?>
+        <br/>
+    </p>
+</body>
+
+</html>
