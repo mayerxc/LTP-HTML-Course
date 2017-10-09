5e2c047016a08bad7ba1ede733036b6879d11e17
Chris Mayer <mayerxc@gmail.com>
2017-10-09 10:28:03 -0400

    finishing activity 2, lab 8 php

--- ltp_php_course/08_lab/08_lab_activity2.php
+++ ltp_php_course/08_lab/08_lab_activity2.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Tax Calculator</title>
+        <title>Calculate Student Scores</title>
         
     </head>
     <body>
-        <h1>Calculate test Scores for students!</h1>
-        <form method="post" action="08_lab_activity2.php">
-            <label for="name">Full Name</label>
-            <input name="name" id="name" type="text"/>
-            <label for="income">Income</label>
-            <input name="income" id="income" type="text"/>
-            <button type="submit">Calculate tax owed</button>
-        </form>
+        <h1>Calculate test Scores for students</h1>
+        
         <?php
             $students = ["Smith"=>["Tests"=>[98,75,100, 91]],
                          "Johnson"=>["Tests"=>[67,88, 85, 81]], 
@@ -31,12 +25,64 @@
                                 [88,82,55,82],
                                 [84,77,81,90],
                                 [84,81,79,100]]];
+            
+            
+            
+            print_r($tests);
+            print("<br/>");
+            print("<br/>The above array is used to calculate the following data:<br/>");
+            print("<br/>");
+        
+            //for loops displaying the test average
+            $sum = 0;
+            for ( $x = 0; $x < count($tests["Tests"][$x]); $x++ ) {
+                //make test number not at index 0;
+                $testNumber = $x + 1;
+                print("Test number: $testNumber");
+                print("<br/>");
+                for ($y = 0; $y < count($tests["Tests"]); $y++) {
+                    //Test score for each individual test
+                    print("Test score: " . $tests["Tests"][$y][$x]);
+                    print("<br/>");
+                    //add up each test
+                    $sum = $tests["Tests"][$y][$x] + $sum;
+                }
+                //calculate average from the sum in nested loop
+                $average = $sum / count($tests["Tests"]);
+                print("Average test score for Test Number $testNumber is: $average");
+                print("<br/>");
+                print("<br/>");
+                //reset sum for next average
+                $sum = 0;
+            }
+        
+            print("<strong>Each student's average test score:</strong>");
+            print("<br/>");
             print_r($students);
             print("<br/>");
-            print( count( $tests["Tests"] ) . "<br/>");
-            for ($x = 0; $x < count($tests["Tests"]); $x++) {
+            print("<br/>The above array is used to calculate the student's average score on all four tests:<br/>");
+            print("<br/>");
+        
+            
+            foreach ($students as $key => $value) {
+                //Name of each student
+                echo("Student: $key");
+                print("<br/>");
                 
+                $sum = 0;
+                for ($i=0; $i < count($value["Tests"]); $i++) {
+                    //get the sum for all tests of each student
+                    $sum = $value["Tests"][$i] + $sum;
+                }
+                //use sum from for loop to calculate average for each student
+                $average = $sum / count($value["Tests"]);
+                print("$key's average test score is: $average");
+                print("<br/>");
+                print("<br/>");
+                //next student
             }
+        
+            
         
         ?>
     </body>
