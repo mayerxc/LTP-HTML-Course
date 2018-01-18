86fec87eb439efaa50ed562291acc0558e156dcb
Chris Mayer <mayerxc@gmail.com>
2018-01-18 11:55:37 -0500

    add solutions to jQuery lab 6

---
+++ ltp_jQuery_course/06_lab/Sample/data.php
@@ -0,0 +1,10 @@
+<?php
+	
+	$username = $_POST['USERNAME'];
+	$email = $_POST['EMAIL'];
+	$password = $_POST['PASSWORD'];
+	
+	// We won't do anything with this data. This is just to show that you have passed data properly.
+	
+	return 'Success';
+?>
